cb0393965c775da9265d7f98512020d9409b6141
Surya Pratap Badal <suryapratapbadal@Suryas-MacBook-Air.local>
2020-09-09 20:43:17 +0530

    TODO APP: update

--- src/InputTodo.tsx
+++ src/InputTodo.tsx
@@ -24,7 +24,7 @@ const InputTodo: React.FC<props> = React.memo(props => {
             <span>
                 <input type="text" value={value} placeholder="Please enter todo item" name="todo" onChange={handleChange} />
             </span>
-            <span style={{ marginLeft: 10, cursor: value.length > 0 ? "pointer" : "auto", pointerEvents: value.length > 0 ? "auto" : "none" }}>
+            <span style={{ marginLeft: 10, cursor: value.length > 0 ? "pointer" : "auto", pointerEvents: value.length > 0 ? "auto" : "none", opacity: value.length > 0 ? 1 : 0.5}}>
                 <button onClick={handleClick} >Add ToDo</button>
             </span>
 
